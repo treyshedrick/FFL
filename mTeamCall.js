8ac557fa3799f4ac649908fd133f8ea02c758e40
treyshedrick <treyshedrick@gmail.com>
2020-09-08 13:31:10 -0500

    append season records

--- mTeamCall.js
+++ mTeamCall.js
@@ -6,8 +6,20 @@ $(document).ready(function(){
             dataType: "json",
             success: function(mteam)
             {
-                $('#teamName').append(mteam.teams[0].location+' '+ mteam.teams[0].nickname);
-                $('#prjRank').append(mteam.teams[0].currentProjectedRank);
+                var team = mteam.teams[0];
+                var teamName = team.location+' '+ team.nickname;
+                var prjRank = team.currentProjectedRank;
+                var overallRecord = team.record.overall;
+                var awayRecord = team.record.away;
+                var homeRecord = team.record.home;
+                var divisionRecord = team.record.division;
+                
+                $('#teamName').append(teamName);
+                $('#prjRank').append(prjRank);
+                $('#overall').append(overallRecord.wins + "-" + overallRecord.losses + "-" + overallRecord.ties);
+                $('#home').append(homeRecord.wins + "-" + homeRecord.losses + "-" + homeRecord.ties);
+                $('#away').append(awayRecord.wins + "-" + awayRecord.losses + "-" + awayRecord.ties);
+                $('#division').append(divisionRecord.wins + "-" + divisionRecord.losses + "-" + divisionRecord.ties);
             },
             error: function(err)
             {
