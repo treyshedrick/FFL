6e2170d728fafc895de30310bf13a2c00c1f31b4
treyshedrick <treyshedrick@gmail.com>
2020-09-17 16:15:11 -0500

    Add more team data

--- mTeamCall.js
+++ mTeamCall.js
@@ -8,18 +8,25 @@ $(document).ready(function(){
             {
                 var team = mteam.teams[0];
                 var teamName = team.location+' '+ team.nickname;
-                var prjRank = team.currentProjectedRank;
                 var overallRecord = team.record.overall;
                 var awayRecord = team.record.away;
                 var homeRecord = team.record.home;
                 var divisionRecord = team.record.division;
                 
                 $('#teamName').append(teamName);
-                $('#prjRank').append(prjRank);
+                $('#playoffSeed').append(team.playoffSeed);
+                $('#prjRank').append(team.currentProjectedRank);
                 $('#overall').append(overallRecord.wins + "-" + overallRecord.losses + "-" + overallRecord.ties);
                 $('#home').append(homeRecord.wins + "-" + homeRecord.losses + "-" + homeRecord.ties);
                 $('#away').append(awayRecord.wins + "-" + awayRecord.losses + "-" + awayRecord.ties);
                 $('#division').append(divisionRecord.wins + "-" + divisionRecord.losses + "-" + divisionRecord.ties);
+                $('#pointsFor').append(overallRecord.pointsFor);
+                $('#pointsAgainst').append(overallRecord.pointsAgainst);
+                
+                if(overallRecord.streakType != null & overallRecord.streakType == "WIN" || overallRecord.streakType == "LOSE")
+                {
+                    $('#winStreak').append(overallRecord.streakLength + " Game " + overallRecord.streakType.charAt(0) + " Streak");
+                }
             },
             error: function(err)
             {
