9c080f535c842df000e7ad1df7056a751352d191
treyshedrick <treyshedrick@gmail.com>
2020-12-03 20:29:11 -0600

    show page after ajax call is completed.

--- mTeamCall.js
+++ mTeamCall.js
@@ -35,6 +35,7 @@ $(document).ready(function(){
                     pointsDiff = "+" + pointsDiff;
                 }
                 $('#pointsDifferential').append(pointsDiff);
+                $('body').show();
             },
             error: function(err)
             {
